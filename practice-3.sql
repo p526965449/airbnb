9acee92d986962f6cd372aaeefaa8ae5a3fc423b
Praveen Renganathan <praveenmanickam.renganathan@kellogg.northwestern.edu>
2024-01-16 01:18:32 +0000

    Answers

--- practice-3.sql
+++ practice-3.sql
@@ -1,5 +1,5 @@
 -- What are all the neighborhoods, alphabetically?
-
+SELECT neighborhood FROM listings GROUP BY neighborhood;
 -- +------------------------+
 -- | Albany Park            |
 -- | Archer Heights         |
